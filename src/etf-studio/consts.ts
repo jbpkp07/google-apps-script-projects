261a56c145dd428651069299d265f75e59566a7a
jbpkp07 <jeremy.w.barnes@gmail.com>
2025-10-15 00:22:36 -0700

    updates fetching etfs

--- src/etf-studio/consts.ts
+++ src/etf-studio/consts.ts
@@ -27,12 +27,13 @@ const FETCHING_SHEET_NAME = "Fetching";
 const IS_FETCHING_TIME_TRIGGER_ENABLED_CELL_NAME = "B3";
 const LAST_FETCHED_TIME_CELL_NAME = "B4";
 
-const TICKERS = ["QQQM", "SPMO", "SPY", "FDVV", "MGV", "XMMO", "SPMD", "RWK", "XSMO", "RWJ", "AVUV"] as const satisfies Tickers;
-const COLUMNS = ["price", "change", "volume", "high52", "high52ch"] as const satisfies WatchListColumnNames;
-const SYMBOLS = TICKERS.map((ticker) => "!" + ticker) as Symbols;
+const TICKERS = ["QQQM", "SPMO", "SPLG", "MGV", "XMMO", "RWJ", "AVUV"] as const satisfies Tickers;
 
-const URL_COLUMNS = COLUMNS.join(",");
+const SYMBOLS = TICKERS.map((ticker) => "!" + ticker) as Symbols;
+const COLUMNS = ["price", "change", "volume", "high52", "high52ch"] as const satisfies WatchListColumnNames;
+
 const URL_SYMBOLS = SYMBOLS.join(",");
+const URL_COLUMNS = COLUMNS.join(",");
 
 const DAYTIME_PRICES_URL = `https://stockanalysis.com/api/quotes/prices?s=${URL_SYMBOLS}`;
 const WATCH_LIST_DATA_URL = `https://stockanalysis.com/api/watchlist?symbols=${URL_SYMBOLS}&columns=${URL_COLUMNS}`;
@@ -41,13 +42,9 @@ const WATCH_LIST_DATA_URL = `https://stockanalysis.com/api/watchlist?symbols=${U
 const ETF_FETCHING_TABLE_CELL_NAMES = {
     QQQM: { name: "B28", price: "E28", changePercent: "F28", volume: "G28", high52Price: "H28", high52ChangePercent: "I28" },
     SPMO: { name: "B29", price: "E29", changePercent: "F29", volume: "G29", high52Price: "H29", high52ChangePercent: "I29" },
-    SPY:  { name: "B30", price: "E30", changePercent: "F30", volume: "G30", high52Price: "H30", high52ChangePercent: "I30" },
-    FDVV: { name: "B31", price: "E31", changePercent: "F31", volume: "G31", high52Price: "H31", high52ChangePercent: "I31" },
-    MGV:  { name: "B32", price: "E32", changePercent: "F32", volume: "G32", high52Price: "H32", high52ChangePercent: "I32" },
-    XMMO: { name: "B33", price: "E33", changePercent: "F33", volume: "G33", high52Price: "H33", high52ChangePercent: "I33" },
-    SPMD: { name: "B34", price: "E34", changePercent: "F34", volume: "G34", high52Price: "H34", high52ChangePercent: "I34" },
-    RWK:  { name: "B35", price: "E35", changePercent: "F35", volume: "G35", high52Price: "H35", high52ChangePercent: "I35" },
-    XSMO: { name: "B36", price: "E36", changePercent: "F36", volume: "G36", high52Price: "H36", high52ChangePercent: "I36" },
-    RWJ:  { name: "B37", price: "E37", changePercent: "F37", volume: "G37", high52Price: "H37", high52ChangePercent: "I37" },
-    AVUV: { name: "B38", price: "E38", changePercent: "F38", volume: "G38", high52Price: "H38", high52ChangePercent: "I38" }
+    SPLG: { name: "B30", price: "E30", changePercent: "F30", volume: "G30", high52Price: "H30", high52ChangePercent: "I30" },
+    MGV:  { name: "B31", price: "E31", changePercent: "F31", volume: "G31", high52Price: "H31", high52ChangePercent: "I31" },
+    XMMO: { name: "B32", price: "E32", changePercent: "F32", volume: "G32", high52Price: "H32", high52ChangePercent: "I32" },
+    RWJ:  { name: "B33", price: "E33", changePercent: "F33", volume: "G33", high52Price: "H33", high52ChangePercent: "I33" },
+    AVUV: { name: "B34", price: "E34", changePercent: "F34", volume: "G34", high52Price: "H34", high52ChangePercent: "I34" }
 } as const satisfies ETFFetchingTableCellNames;
