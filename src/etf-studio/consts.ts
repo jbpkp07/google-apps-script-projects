40af0a86018289559aacab5a17017c3040a13c0a
jbpkp07 <jeremy.w.barnes@gmail.com>
2025-11-02 01:02:53 -0800

    changes SPLG to SPYM

--- src/etf-studio/consts.ts
+++ src/etf-studio/consts.ts
@@ -7,7 +7,7 @@ const FETCHING_SHEET_NAME = "Fetching";
 const IS_FETCHING_TIME_TRIGGER_ENABLED_CELL_NAME = "B3";
 const LAST_FETCHED_TIME_CELL_NAME = "B4";
 
-const TICKERS = ["QQQM", "SPMO", "SPLG", "MGV", "XMMO", "RWJ", "AVUV", "SPY", "AAPL", "AMZN"] as const satisfies Tickers;
+const TICKERS = ["QQQM", "SPMO", "SPYM", "MGV", "XMMO", "RWJ", "AVUV", "SPY", "AAPL", "AMZN"] as const satisfies Tickers;
 
 const SYMBOLS = TICKERS.map((ticker) => `!${ticker}` as const) satisfies TickerSymbols;
 const COLUMNS = ["price", "change", "volume", "high52", "high52ch"] as const satisfies WatchListColumnNames;
@@ -22,7 +22,7 @@ const WATCH_LIST_DATA_URL = `${DOMAIN}/api/watchlist?symbols=${URL_SYMBOLS}&colu
 const ETF_FETCHING_TABLE_CELL_NAMES = {
     QQQM: { name: "B28", price: "E28", changePercent: "F28", volume: "G28", high52Price: "H28", high52ChangePercent: "I28" },
     SPMO: { name: "B29", price: "E29", changePercent: "F29", volume: "G29", high52Price: "H29", high52ChangePercent: "I29" },
-    SPLG: { name: "B30", price: "E30", changePercent: "F30", volume: "G30", high52Price: "H30", high52ChangePercent: "I30" },
+    SPYM: { name: "B30", price: "E30", changePercent: "F30", volume: "G30", high52Price: "H30", high52ChangePercent: "I30" },
     MGV:  { name: "B31", price: "E31", changePercent: "F31", volume: "G31", high52Price: "H31", high52ChangePercent: "I31" },
     XMMO: { name: "B32", price: "E32", changePercent: "F32", volume: "G32", high52Price: "H32", high52ChangePercent: "I32" },
     RWJ:  { name: "B33", price: "E33", changePercent: "F33", volume: "G33", high52Price: "H33", high52ChangePercent: "I33" },
@@ -41,7 +41,7 @@ const LAST_SCRAPED_TIME_CELL_NAME = "B4";
 const ETF_SCRAPING_TABLE_ROWS = {
     QQQM: { url: `${DOMAIN}/etf/qqqm`,    uniqueUrlCellName: "F14", scrapeResultsRangeName: "M14:O14" },
     SPMO: { url: `${DOMAIN}/etf/spmo`,    uniqueUrlCellName: "F15", scrapeResultsRangeName: "M15:O15" },
-    SPLG: { url: `${DOMAIN}/etf/splg`,    uniqueUrlCellName: "F16", scrapeResultsRangeName: "M16:O16" },
+    SPYM: { url: `${DOMAIN}/etf/spym`,    uniqueUrlCellName: "F16", scrapeResultsRangeName: "M16:O16" },
     MGV:  { url: `${DOMAIN}/etf/mgv`,     uniqueUrlCellName: "F17", scrapeResultsRangeName: "M17:O17" },
     XMMO: { url: `${DOMAIN}/etf/xmmo`,    uniqueUrlCellName: "F18", scrapeResultsRangeName: "M18:O18" },
     RWJ:  { url: `${DOMAIN}/etf/rwj`,     uniqueUrlCellName: "F19", scrapeResultsRangeName: "M19:O19" },
--- src/etf-studio/types/types.ts
+++ src/etf-studio/types/types.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-type Tickers = readonly ["QQQM", "SPMO", "SPLG", "MGV", "XMMO", "RWJ", "AVUV", "SPY", "AAPL", "AMZN"];
+type Tickers = readonly ["QQQM", "SPMO", "SPYM", "MGV", "XMMO", "RWJ", "AVUV", "SPY", "AAPL", "AMZN"];
 type Ticker = NonNullable<Tickers[number]>;
 
 type TickerSymbols = readonly `!${Ticker}`[];
